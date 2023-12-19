67c3b3d53e0b6b5879707d94d696e38393560571
Ramon Kits <ramon@e-tales.nl>
2023-12-19 18:50:54 +0100

    added trinomino and edited .gitignore

--- week 3/start_triomino.py
+++ week 3/start_triomino.py
@@ -47,7 +47,7 @@ def make_matrix(triominoes):
 def prepare(mx):
     # note that when applying alg-x we're only interested in 1's
     # so we add 2 lists that define where the 1's are
-    rows = mx    
+    rows = mx
     # note that zip(*b) is the transpose of b
     cols = [list(i) for i in zip(*rows)]
 
@@ -55,7 +55,7 @@ def prepare(mx):
     print()
 
     def find_ones(rows):
-        # returns indexes in rows where the ondes are
+        # returns indexes in rows where the ones are
         # example: [[0, 3], [1, 3], [1, 2], [2]]
         lv_row_has_1_at = []
         for row in rows:
@@ -85,9 +85,12 @@ def cover(r, row_valid, col_valid, row_has_1_at, col_has_1_at):
     # given a row r:
     #   cover all cols that have a 1 in row r
     #   cover all rows r' that intersect/overlap with row r
-    # returns row_valid, col_valid
-
-    pass
+    row_valid[r] = 0
+    for c in row_has_1_at[r]:
+        col_valid[c] = 0
+        for r1 in col_has_1_at[c]:
+            row_valid[r1] = 0
+    return row_valid, col_valid
 
 def print_solution(solution, row_has_1_at):
     # place triominoes in matrix D 3 rows x 4 cols
@@ -111,8 +114,27 @@ def print_solution(solution, row_has_1_at):
 
 def solve(row_valid, col_valid, row_has_1_at, col_has_1_at, solution):
     # using Algoritm X, find all solutions (= set of rows) given valid/uncovered rows and cols
-    pass
-
+    # find column with fewest ones
+    min_ones = NR_OF_ROWS + 1
+    min_col = -1
+    for i in range(len(col_valid)):
+        if col_valid[i] == 1:
+            if col_has_1_at[i] != []:
+                if len(col_has_1_at[i]) < min_ones:
+                    min_ones = len(col_has_1_at[i])
+                    min_col = i
+    # find first row with a 1 in min_col
+    for r in col_has_1_at[min_col]:
+        if row_valid[r] == 1:
+            # cover all rows and cols that intersect/overlap with row r
+            row_valid1, col_valid1 = cover(r, row_valid.copy(), col_valid.copy(), row_has_1_at, col_has_1_at)
+            solution1 = solution.copy()
+            solution1.append(r)
+            # if there are no more 1's left, we have a solution
+            if sum(row_valid1) == 0 and sum(col_valid1) == 0:
+                print_solution(solution1, row_has_1_at)
+            else:
+                solve(row_valid1, col_valid1, row_has_1_at, col_has_1_at, solution1)
 
 mx = make_matrix(triominoes)
 
